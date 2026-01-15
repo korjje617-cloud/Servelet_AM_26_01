955171ddbe792e8fa3d6f364b5894fa4bdf7f30b
korjje617 <korjje617@gmail.com>
2026-01-15 09:35:57 +0900

    게시글 상세보기, 형식없음

--- src/main/java/com/KoreaIT/java/AM_jsp/ArticleDetailServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/ArticleDetailServlet.java
@@ -41,6 +41,14 @@ public class ArticleDetailServlet extends HttpServlet {
 			response.getWriter().append("연결 성공");
 
 			DBUtil dbUtil = new DBUtil(request, response);
+			
+			int id = Integer.parseInt(request.getParameter("id"));
+
+			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
+
+			Map<String, Object> articleRow = dbUtil.selectRow(conn, sql);
+
+			request.setAttribute("articleRow", articleRow);
 
 			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
 			
--- src/main/webapp/jsp/article/detail.jsp
+++ src/main/webapp/jsp/article/detail.jsp
@@ -3,15 +3,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 
+<%
+Map<String, Object> articleRow = (Map<String, Object>) request.getAttribute("articleRow");
+%>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>상세 페이지</title>
+<title>게시글 상세보기</title>
 </head>
 <body>
 
-	<h1>상세 페이지</h1>
+	<h1>게시글 상세보기</h1>
+
+	<ul>
+		<%=articleRow %>
+	</ul>
 
 </body>
 </html>
